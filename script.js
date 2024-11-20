4fe635d9181fc33251b24f27723f84418900171d
DarysFox <darya.khsv@gmail.com>
2024-11-20 18:07:52 +0300

    commit4 - moving

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let context;
 
 let unicornWidth = 46;
 let unicornHeight = 46;
-let unicornY = unicornWidth / 2 - unicornWidth / 2;
-let unicornX = (unicornHeight * 7) / 8 - unicornHeight;
+let unicornX = unicornWidth / 2 - unicornWidth / 2;
+let unicornY = (unicornHeight * 7) / 8 - unicornHeight;
 let unicornRightImg;
 let unicornLeftImg;
+
+let velosityX = 0;
 
 let unicorn = {
   img: null,
@@ -22,5 +24,48 @@ window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
   board.width = boardWidth;
-  context = boad.getContext("2d");
+  context = board.getContext("2d");
+
+  // context.fillStyle = "pink";
+  // context.fillRect(unicorn.x, unicorn.y, unicorn.width, unicorn.height);
+
+  unicornRightImg = new Image();
+  unicornRightImg.src = "images/unicorn-right.png";
+  unicorn.img = unicornRightImg;
+
+  unicornRightImg.onload = function () {
+    context.drawImage(
+      unicorn.img,
+      unicorn.x,
+      unicorn.y,
+      unicorn.width,
+      unicorn.height
+    );
+  };
+  unicornLeftImg = new Image();
+  unicornLeftImg.src = "images/unicorn-left.png";
+
+  requestAnimationFrame(update);
+  document.addEventListener("keydown", moveUNnicorn);
+
+  function update() {
+    requestAnimationFrame(update);
+    context.drawImage(
+      unicorn.img,
+      unicorn.x,
+      unicorn.y,
+      unicorn.width,
+      unicorn.height
+    );
+  }
+
+  function moveUNnicorn(e) {
+    if (e.code == "ArrowRight" || e.code == "KeyD") {
+      velosityX = 4;
+      unicorn.img = unicorn.unicornRightImg;
+    } else if (e.code == "ArrowLeft" || e.code == "KeyA") {
+      velosityX = -4;
+      unicorn.img = unicorn.unicornLeftImg;
+    }
+  }
 };
