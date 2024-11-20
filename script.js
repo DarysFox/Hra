7c72bb81e8f0fcbd13ada5aaed608d8c65290eb6
DarysFox <darya.khsv@gmail.com>
2024-11-20 16:23:16 +0300

    commit3 - unicorn

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ let boardWidth = 360;
 let boardHeight = 575;
 let context;
 
+let unicornWidth = 46;
+let unicornHeight = 46;
+let unicornY = unicornWidth / 2 - unicornWidth / 2;
+let unicornX = (unicornHeight * 7) / 8 - unicornHeight;
+let unicornRightImg;
+let unicornLeftImg;
+
+let unicorn = {
+  img: null,
+  x: unicornX,
+  y: unicornY,
+  width: unicornWidth,
+  height: unicornHeight,
+};
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
