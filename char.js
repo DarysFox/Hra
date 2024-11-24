a2ff2c682ca1d5899c938988afa3a9452557041b
DarysFox <darya.khsv@gmail.com>
2024-11-24 15:13:17 +0300

    commit12 - hiden name

--- char.js
+++ char.js
@@ -1,11 +1,21 @@
 const goldyCard = document.getElementById("goldy-card");
 const questionImg = document.getElementById("question-img");
 const goldyImg = document.getElementById("goldy-img");
+const goldyName = document.getElementById("goldy-name");
+const hideName = document.getElementById("hide");
 const unlockButton = document.getElementById("unlock-goldy");
 
 unlockButton.addEventListener("click", () => {
   questionImg.classList.add("hidden");
   goldyImg.classList.remove("hidden");
+  hideName.classList.add("hidden");
+  goldyName.classList.remove("hidden");
 
   goldyCard.classList.remove("locked");
+
+  goldyCard.classList.add("glow");
+
+  setTimeout(() => {
+    goldyCard.classList.remove("glow");
+  }, 2000);
 });
